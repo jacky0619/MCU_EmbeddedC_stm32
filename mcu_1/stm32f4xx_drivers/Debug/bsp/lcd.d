4cb6897c640d658b4f2de08e4b8e476085654a1c
jacky0619 <32379543+jacky0619@users.noreply.github.com>
2021-09-12 17:09:21 +0800

    add image file

---
+++ mcu_1/stm32f4xx_drivers/Debug/bsp/lcd.d
@@ -0,0 +1,24 @@
+bsp/lcd.o: ../bsp/lcd.c ../bsp/lcd.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h \
+ C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h
+
+../bsp/lcd.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
+
+C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:
--- mcu_1/stm32f4xx_drivers/Debug/bsp/subdir.mk
+++ mcu_1/stm32f4xx_drivers/Debug/bsp/subdir.mk
@@ -5,16 +5,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../bsp/ds1307.c 
+../bsp/ds1307.c \
+../bsp/lcd.c 
 
 OBJS += \
-./bsp/ds1307.o 
+./bsp/ds1307.o \
+./bsp/lcd.o 
 
 C_DEPS += \
-./bsp/ds1307.d 
+./bsp/ds1307.d \
+./bsp/lcd.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 bsp/ds1307.o: ../bsp/ds1307.c bsp/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F407VGTx -DSTM32F407G_DISC1 -DDEBUG -c -I"C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc" -I"C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/bsp" -I"C:/ST/STM32CubeIDE_1.6.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.9-2020-q2-update.win32_1.5.0.202011040924/tools/arm-none-eabi/include/newlib-nano" -O0 -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"bsp/ds1307.d" -MT"$@"  -mfloat-abi=soft -mthumb -o "$@"
+bsp/lcd.o: ../bsp/lcd.c bsp/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F407VGTx -DSTM32F407G_DISC1 -DDEBUG -c -I"C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/drivers/inc" -I"C:/STM32/MCU1_course/mcu_1/stm32f4xx_drivers/bsp" -I"C:/ST/STM32CubeIDE_1.6.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.9-2020-q2-update.win32_1.5.0.202011040924/tools/arm-none-eabi/include/newlib-nano" -O0 -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"bsp/lcd.d" -MT"$@"  -mfloat-abi=soft -mthumb -o "$@"
 
